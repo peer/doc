71770d91bc5f3be2b432d832596b96a4f14a1cac
Mitar <mitar.git@tnode.com>
2017-11-11 02:47:21 -0800

    Load document.

--- client/components/document.vue
+++ client/components/document.vue
@@ -1,18 +1,50 @@
 <template>
-  <p>{{$route.params.documentId}}</p>
+  <v-layout v-if="document" row>
+    <v-flex xs12>
+      <v-card>
+        <v-card-text>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+  <not-found v-else-if="$subscriptionsReady()"></not-found>
 </template>
 
 <script>
   import {RouterFactory} from 'meteor/akryum:vue-router2';
 
-  const component = {};
+  import {Document} from '/lib/document';
+
+  const component = {
+    props: {
+      documentId: {
+        type: String,
+        required: true
+      }
+    },
+
+    created() {
+      this.$autorun((computation) => {
+        this.$subscribe('Document.one', {documentId: this.documentId});
+      });
+    },
+
+    computed: {
+      document() {
+        return Document.documents.findOne({
+          _id: this.documentId
+        });
+      }
+    }
+  };
 
   RouterFactory.configure((factory) => {
     factory.addRoutes([
       {
         component,
         path: '/document/:documentId',
-        name: 'document'
+        name: 'document',
+        props: true
       },
     ]);
   });
--- server/document.js
+++ server/document.js
@@ -1,4 +1,4 @@
-import {check} from 'meteor/check';
+import {check, Match} from 'meteor/check';
 import {Meteor} from 'meteor/meteor';
 
 import {Document} from '/lib/document';
@@ -9,4 +9,14 @@ Meteor.publish('Document.list', function (args) {
   this.enableScope();
 
   return Document.documents.find({}, {fields: Document.PUBLISH_FIELDS()});
+});
+
+Meteor.publish('Document.one', function (args) {
+  check(args, {
+    documentId: Match.DocumentId
+  });
+
+  return Document.documents.find({
+    _id: args.documentId
+  }, {fields: Document.PUBLISH_FIELDS()});
 });
