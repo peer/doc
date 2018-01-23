2be3a55fceecfcea99c70f5c626c1ee5e0d3abcf
vughelli <vduk225@gmail.com>
2018-01-23 13:01:47 -0300

    Split list item on Enter
    Enter key mapped to splitListItem command.
    
    See #1
    
    Signed-off-by: vughelli <vduk225@gmail.com>

--- client/components/editor.vue
+++ client/components/editor.vue
@@ -149,8 +149,9 @@
         schema: peerDocSchema,
         plugins: [
           keymap({
+            Enter: splitListItem(peerDocSchema.nodes.list_item),
             'Mod-z': undo,
-            'Mod-y': redo, // TODO: shift+mod+z
+            'Shift-Mod-z': redo,
           }),
           keymap(baseKeymap),
           dropCursor(),
