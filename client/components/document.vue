40e334983a42e30bb67a5dc4640e48c5f43ed626
Guillermo Peralta Scura <gperaltascura@gmail.com>
2018-02-14 15:36:26 -0300

    Update to Vuetify 1.0

--- client/components/document.vue
+++ client/components/document.vue
@@ -50,26 +50,22 @@
         <v-layout row class="mt-3">
           <v-flex>
             <v-tabs grow light>
-              <v-tabs-bar class="grey lighten-4">
-                <v-tabs-item ripple href="#comments" class="primary--text">Comments</v-tabs-item>
-                <v-tabs-item ripple href="#chat" class="primary--text"><v-badge><span slot="badge">4</span>Chat</v-badge></v-tabs-item>
-                <v-tabs-item ripple href="#history" class="primary--text">History</v-tabs-item>
-                <v-tabs-slider color="primary" />
-              </v-tabs-bar>
-              <v-tabs-items>
-                <v-tabs-content id="comments">
-                  <v-card>
-                    <v-card-text>
-                      Comment 1.
-                    </v-card-text>
-                  </v-card>
-                  <v-card>
-                    <v-card-text>
-                      Comment 2.
-                    </v-card-text>
-                  </v-card>
-                </v-tabs-content>
-              </v-tabs-items>
+              <v-tab ripple href="#comments" class="primary--text">Comments</v-tab>
+              <v-tab ripple href="#chat" class="primary--text"><v-badge><span slot="badge">4</span>Chat</v-badge></v-tab>
+              <v-tab ripple href="#history" class="primary--text">History</v-tab>
+              <!-- <v-tabs-slider color="primary" /> -->
+              <v-tab-item id="comments">
+                <v-card>
+                  <v-card-text>
+                    Comment 1.
+                  </v-card-text>
+                </v-card>
+                <v-card>
+                  <v-card-text>
+                    Comment 2.
+                  </v-card-text>
+                </v-card>
+              </v-tab-item>
             </v-tabs>
           </v-flex>
         </v-layout>
