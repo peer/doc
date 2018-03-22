ace4825d55fff6219a4589d9596d645aad7d8a9e
Mitar <mitar.git@tnode.com>
2018-03-22 11:24:18 -0700

    Style.

--- server/documents/cursor.js
+++ server/documents/cursor.js
@@ -5,14 +5,14 @@ import {Cursor} from '/lib/documents/cursor';
 import {User} from '/lib/documents/user';
 import randomColor from 'randomcolor';
 
-// Server-side only method, so we are not using ValidatedMethod.
+// Server-side only methods, so we are not using ValidatedMethod.
 Meteor.methods({
-
   'Cursor.remove'(args) {
     check(args, {
       contentKey: Match.DocumentId,
       clientId: Match.DocumentId,
     });
+
     Cursor.documents.remove({
       contentKey: args.contentKey,
       clientId: args.clientId,
