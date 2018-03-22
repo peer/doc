5ae3fc0d279ceeb7e41947272e36590af25b442c
Guillermo Peralta Scura <gperaltascura@gmail.com>
2018-03-22 11:23:03 -0300

    Add decrypt handle to Embed methods
    See #38.

--- server/embed.js
+++ server/embed.js
@@ -1,7 +1,29 @@
 import {Accounts} from 'meteor/accounts-base';
 import {check, Match} from 'meteor/check';
 import {Meteor} from 'meteor/meteor';
+import crypto from 'crypto';
 
+const baseToMap = {
+  '-': '+',
+  _: '/',
+  '.': '=',
+};
+
+function decrypt(tokenBase, keyHex) {
+  const token = Buffer.from(tokenBase.replace(/[-_.]/g, (c) => {
+    return baseToMap[c];
+  }), 'base64');
+  const iv = token.slice(0, 12);
+  const authTag = token.slice(12, 28);
+  const ciphertext = token.slice(28);
+  const decipher = crypto.createDecipheriv('aes-128-gcm', Buffer.from(keyHex, 'hex'), iv);
+  decipher.setAuthTag(authTag);
+  const json = Buffer.concat([decipher.update(ciphertext), decipher.final()]).toString('utf8');
+  return JSON.parse(json);
+}
+
+// const keyHex = crypto.randomBytes(16).toString('hex');
+const keyHex = '960d1591462f289d6b5a748aca0eb9e4';
 // A special case which is not using ValidatedMethod because client side
 // differs a lot from the server side and there is no client stub.
 Meteor.methods({
@@ -20,7 +42,7 @@ Meteor.methods({
 
       const {userToken} = args;
 
-      console.log(userToken);
+      decrypt(userToken, keyHex);
 
       throw Error("Not yet implemented");
     });
