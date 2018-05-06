410cdac7c76fbad1a1428078386a72cca518acad
Mitar <mitar.git@tnode.com>
2018-05-06 13:39:54 -0700

    Revert "Add style linting."
    This reverts commit 960fa6f944a30eb1bb0e1436dd721d42e67603af.

--- .postcssrc.js
+++ .postcssrc.js
@@ -2,8 +2,6 @@ module.exports = (ctx) => {
   if (ctx.meteor) {
     const config = {
       plugins: {
-        // We care about linting issues in files made by developers, so this is first.
-        'stylelint': {},
         // To import CSS files from NPM packages.
         'postcss-easy-import': {},
       },
@@ -19,27 +17,10 @@ module.exports = (ctx) => {
       };
     }
 
-    // Reporter is last.
-    config.plugins['postcss-reporter'] = {
-      'clearReportedMessages': true,
-    };
-
     return config;
   }
   // In our case, this is done when processing <style> tags inside Vue components.
   else {
-    return {
-      /*
-      TODO: Disabled for now.
-            See: https://github.com/meteor-vue/vue-meteor/pull/317
-      plugins: {
-        // We just do linting here and leave the rest to default plugins.
-        'stylelint': {},
-        'postcss-reporter': {
-          'clearReportedMessages': true,
-        },
-      ],
-      */
-    };
+    return {};
   }
 };
