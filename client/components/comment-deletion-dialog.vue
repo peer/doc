36b1c6361e72856c518c2569ed873326e6c01b16
vughelli <vduk225@gmail.com>
2018-05-16 16:41:31 -0400

    Added comment-deletion-dialog component.
    Signed-off-by: vughelli <vduk225@gmail.com>

---
+++ client/components/comment-deletion-dialog.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <v-dialog
+      v-model="show"
+      hide-overlay
+      max-width="500px"
+    >
+      <v-card>
+        <v-card-title>
+          <div v-if="dialogType === 'thread'">
+            Delete Thread?
+          </div>
+          <div v-else>
+            Delete Comment?
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn
+            color="secondary"
+            flat
+            @click="show = false"
+          ><translate>cancel</translate></v-btn>
+          <v-btn
+            color="error"
+            flat
+            @click="deleteComment"
+          ><translate>remove</translate></v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+
+  // @vue/component
+  const component = {
+    props: {
+      dialogType: {
+        type: String,
+        default: 'single',
+      },
+    },
+    data() {
+      return {
+        show: false,
+      };
+    },
+    methods: {
+      deleteComment() {
+        this.$emit('delete-comment');
+        this.show = false;
+      },
+    },
+  };
+
+  export default component;
+</script>
