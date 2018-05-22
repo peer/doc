4d89d38a9e327a4c9a582edf81ecb31b936067cb
vughelli <vduk225@gmail.com>
2018-05-22 11:21:28 -0400

    Fix status migration.
    Signed-off-by: vughelli <vduk225@gmail.com>

--- server/migrations/0001-comment-status.js
+++ server/migrations/0001-comment-status.js
@@ -1,11 +1,9 @@
 import {Comment} from '/lib/documents/comment';
 
 class Migration extends Document.AddRequiredFieldsMigration {
-  constructor() {
-    super();
-    this.name = "Adding status field";
-    this.fields =
-    {status: ''};
+  name = "Adding status field"
+  fields = {
+    status: Comment.STATUS.CREATED,
   }
 }
 
