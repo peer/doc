47cb40914ec921f847e5d35743024461149ac794
vughelli <vduk225@gmail.com>
2018-06-26 14:26:05 -0400

    Commented return false line for the moment.
    If false is returned the transaction will be cancelled, it works fine
    when the title is modified by only one user, but when it's modified
    by two (or more) users at the same time a mismatch could occur. For
    this reason, the return false line is commented.
    
    Signed-off-by: vughelli <vduk225@gmail.com>

--- client/components/utils/title-size-plugin.js
+++ client/components/utils/title-size-plugin.js
@@ -5,13 +5,25 @@ export const titleSizePlugin = (max, message) => {
   let lastOccurrenceAt = 0;
   return new Plugin({
     filterTransaction(tr, state) {
+      // Check if the transaction title length is greater than the limit
       if (tr.doc.content.firstChild.content.size > max) {
         const now = new Date();
-        if (now - lastOccurrenceAt > 1000) {
+
+        // Check if the title is modified in the transaction
+        const replacedTitle = state.doc.content.firstChild.content.size !== tr.doc.content.firstChild.content.size;
+
+        // Show snackbar only when the title is modified.
+        if (replacedTitle && now - lastOccurrenceAt > 1000) {
           Snackbar.enqueue(message, 'warning');
         }
+
         lastOccurrenceAt = now;
-        return false;
+
+        // If false is returned the transaction will be cancelled, it works fine when
+        // the title is modified by only one user, but when it's modified by two (or
+        // more) users at the same time a mismatch could occur. For this reason, the
+        // line below is commented.
+        // return false;
       }
       return true;
     },
