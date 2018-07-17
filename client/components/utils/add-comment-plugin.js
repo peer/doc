64d94986d36233136560ecdff7009badbf063002
Mitar <mitar.git@tnode.com>
2018-07-17 02:08:59 -0700

    Moved code to add-comment-plugin.
    Fixes #131.

--- client/components/utils/add-comment-plugin.js
+++ client/components/utils/add-comment-plugin.js
@@ -1,4 +1,7 @@
-import {Plugin} from "prosemirror-state";
+import {Plugin} from 'prosemirror-state';
+import {MarkType} from 'prosemirror-model';
+
+import {AddHighlightStep, RemoveHighlightStep} from '/lib/transform/highlight_step';
 
 class AddComment {
   constructor(view, vueInstance) {
@@ -71,6 +74,96 @@ class AddComment {
   }
 }
 
+// Add the given mark to the inline content between `from` and `to`.
+function addHighlightMark(tr, from, to, mark) {
+  const removed = [];
+  const added = [];
+  let removing = null;
+  let adding = null;
+
+  tr.doc.nodesBetween(from, to, (node, pos, parent) => {
+    if (!node.isInline) return;
+    const marks = node.marks;
+    if (!mark.isInSet(marks) && parent.type.allowsMarkType(mark.type)) {
+      const start = Math.max(pos, from);
+      const end = Math.min(pos + node.nodeSize, to);
+      const newSet = mark.addToSet(marks);
+
+      for (let i = 0; i < marks.length; i += 1) {
+        if (!marks[i].isInSet(newSet)) {
+          if (removing && removing.to === start && removing.mark.eq(marks[i])) {
+            removing.to = end;
+          }
+          else {
+            removed.push(removing = new RemoveHighlightStep(start, end, marks[i]));
+          }
+        }
+      }
+
+      if (adding && adding.to === start) {
+        adding.to = end;
+      }
+      else {
+        added.push(adding = new AddHighlightStep(start, end, mark));
+      }
+    }
+  });
+
+  removed.forEach((s) => {
+    tr.step(s);
+  });
+  added.forEach((s) => {
+    tr.step(s);
+  });
+  return tr;
+}
+
+// Remove marks from inline nodes between `from` and `to`. When `mark`
+// is a single mark, remove precisely that mark. When it is a mark type,
+// remove all marks of that type. When it is null, remove all marks of
+// any type.
+function removeHighlightMark(tr, from, to, mark = null) {
+  const matched = [];
+  let step = 0;
+  tr.doc.nodesBetween(from, to, (node, pos) => {
+    if (!node.isInline) return;
+    step += 1;
+    let toRemove = null;
+    if (mark instanceof MarkType) {
+      const found = mark.isInSet(node.marks);
+      if (found) toRemove = [found];
+    }
+    else if (mark) {
+      if (mark.isInSet(node.marks)) toRemove = [mark];
+    }
+    else {
+      toRemove = node.marks;
+    }
+    if (toRemove && toRemove.length) {
+      const end = Math.min(pos + node.nodeSize, to);
+      for (let i = 0; i < toRemove.length; i += 1) {
+        const style = toRemove[i];
+        let found;
+        for (let j = 0; j < matched.length; j += 1) {
+          const m = matched[j];
+          if (m.step === step - 1 && style.eq(matched[j].style)) found = m;
+        }
+        if (found) {
+          found.to = end;
+          found.step = step;
+        }
+        else {
+          matched.push({style, from: Math.max(pos, from), to: end, step});
+        }
+      }
+    }
+  });
+  matched.forEach((m) => {
+    tr.step(new RemoveHighlightStep(m.from, m.to, m.style));
+  });
+  return tr;
+}
+
 export default function addCommentPlugin(vueInstance) {
   return new Plugin({
     view(editorView) {
@@ -82,14 +175,14 @@ export default function addCommentPlugin(vueInstance) {
 export function addHighlight(keys, schema, tr, from, to, dispatch) {
   const attrs = {"highlight-keys": keys};
   tr.setMeta("addToHistory", false);
-  return tr.addHighlightMark(from, to, schema.marks.highlight.create(attrs));
+  return addHighlightMark(tr, from, to, schema.marks.highlight.create(attrs));
 }
 
 export function removeHighlight(schema, tr, doc, from, to, dispatch) {
   if (dispatch) {
     if (doc.rangeHasMark(from, to, schema.marks.highlight)) {
       tr.setMeta("addToHistory", false);
-      return tr.removeHighlightMark(from, to, schema.marks.highlight);
+      return removeHighlightMark(tr, from, to, schema.marks.highlight);
     }
   }
   return null;
