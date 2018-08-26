71692b1d54db8bd0bd11df2c7b3028417fcd792a
Mitar <mitar.git@tnode.com>
2018-08-26 05:26:47 -0700

    Added migration for document body field.

---
+++ server/migrations/0007-document-body.js
@@ -0,0 +1,56 @@
+import {extractTitle} from '/lib/utils';
+import {Content} from '/server/documents/content';
+import {Document} from '/server/documents/document';
+
+// This migration is a data migration and not schema migration.
+// We are updating documents, not really changing a schema.
+
+class Migration extends Document.PatchMigration {
+  name = "Regenerating body field";
+
+  forward(documentClass, collection, currentSchema, newSchema) {
+    let count = 0;
+
+    collection.findEach({_schema: currentSchema}, {contentKey: 1}, (document) => {
+      const {doc, version} = Content.getCurrentState(document.contentKey);
+
+      count += collection.update({_schema: currentSchema, _id: document._id}, {
+        $set: {
+          version,
+          _schema: newSchema,
+          body: doc.toJSON(),
+          title: extractTitle(doc),
+        },
+      });
+    });
+
+    const counts = super.forward(documentClass, collection, currentSchema, newSchema);
+    counts.migrated += count;
+    counts.all += count;
+    return counts;
+  }
+
+  backward(documentClass, collection, currentSchema, oldSchema) {
+    let count = 0;
+
+    collection.findEach({_schema: currentSchema}, {contentKey: 1}, (document) => {
+      const {doc, version} = Content.getCurrentState(document.contentKey);
+
+      count += collection.update({_schema: currentSchema, _id: document._id}, {
+        $set: {
+          version,
+          _schema: oldSchema,
+          body: doc.toJSON(),
+          title: extractTitle(doc),
+        },
+      });
+    });
+
+    const counts = super.backward(documentClass, collection, currentSchema, oldSchema);
+    counts.migrated += count;
+    counts.all += count;
+    return counts;
+  }
+}
+
+Document.addMigration(new Migration());
