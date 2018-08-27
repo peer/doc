e24dd380675640870ac1ab9161ecc344cc1df7aa
vughelli <vduk225@gmail.com>
2018-08-27 13:27:53 -0400

    Update merge method.
    Signed-off-by: vughelli <vduk225@gmail.com>

--- server/documents/document.js
+++ server/documents/document.js
@@ -408,185 +408,72 @@ Meteor.methods({
       throw new Meteor.Error('unauthorized', "Unauthorized.");
     }
 
-    // Get forked document.
     const fork = Document.documents.findOne(
       {
         _id: args.documentId,
+        isMerged: {$ne: true},
       },
       {
         fields: {
-          contentKey: 1,
-          forkedAtVersion: 1,
-          forkedFrom: 1,
           _id: 1,
+          title: 1,
+          body: 1,
+          version: 1,
+          contentKey: 1,
+          lastSync: 1,
+          forkedFrom: 1,
         },
       },
     );
 
-    // Get forked document steps.
-    const forkSteps = Content.documents.find(
-      {
-        version: {
-          $gt: fork.forkedAtVersion,
-        },
-        contentKeys: fork.contentKey,
-      },
-      {
-        sort: {
-          version: 1,
-        },
-      },
-    ).fetch()
-    .map((x) => {
-      return Step.fromJSON(schema, x.step);
-    });
-
     // Get original document.
     const original = Document.documents.findOne({
       _id: fork.forkedFrom._id,
+    }, {
+      fields: {
+        _id: 1,
+        contentKey: 1,
+      },
     });
 
-    // Initialize doc and transform.
-    let doc = schema.topNodeType.createAndFill();
-    let transform;
-    let version = 0;
-
-    // Apply all the original document steps to doc.
-    Content.documents.find({
-      contentKeys: original.contentKey,
+    // Add original steps to forked.
+    Content.documents.update({
+      contentKeys: fork.contentKey,
       version: {
-        $gt: version,
+        $gt: fork.lastSync,
       },
     }, {
-      sort: {
-        version: 1,
-      },
-      fields: {
-        step: 1,
-        version: 1,
-      },
-    }).fetch().forEach((content) => {
-      if (content.version > fork.forkedAtVersion) {
-        if (!transform) {
-          transform = new Transform(doc);
-        }
-        transform.step(Step.fromJSON(schema, content.step));
-      }
-
-      const result = Step.fromJSON(schema, content.step).apply(doc);
-
-      if (!result.doc) {
-        // eslint-disable-next-line no-console
-        console.error("Error applying a step.", result.failed);
-        throw new Meteor.Error('invalid-request', "Invalid step.");
-      }
-      doc = result.doc;
-      version = content.version;
-    });
-
-    // Get original document steps that were applied after fork.
-    const originalSteps = Content.documents.find(
-      {
-        version: {
-          $gt: fork.forkedAtVersion,
-        },
+      $addToSet: {
         contentKeys: original.contentKey,
       },
-      {
-        sort: {
-          version: 1,
-        },
-      },
-    ).fetch()
-    .map((x) => {
-      return Step.fromJSON(schema, x.step);
+    }, {
+      multi: true,
     });
-
-    const shouldRebase = transform !== undefined && forkSteps.length > 0;
-
-    if (shouldRebase) {
-      // Revert steps that were applied on the original document after fork.
-      for (let i = transform.steps.length - 1; i >= 0; i -= 1) {
-        const result = transform.steps[i].invert(transform.docs[i]).apply(doc);
-        transform.step(transform.steps[i].invert(transform.docs[i]));
-        if (!result.doc) {
-          // eslint-disable-next-line no-console
-          console.error("Error applying a step.", result.failed);
-          throw new Meteor.Error('invalid-request', "Invalid step.");
-        }
-        doc = result.doc;
-      }
-    }
-    else {
-      transform = new Transform(doc);
-    }
-
-    // Apply all the forked document steps.
-    for (let i = 0; i < forkSteps.length; i += 1) {
-      const result = forkSteps[i].apply(doc);
-      transform.step(forkSteps[i]);
-      if (!result.doc) {
-        // eslint-disable-next-line no-console
-        console.error("Error applying a step.", result.failed);
-        throw new Meteor.Error('invalid-request', "Invalid step.");
-      }
-      doc = result.doc;
-    }
-
-    if (shouldRebase) {
-      // Remap original document steps and apply.
-      for (let i = 0, mapFrom = originalSteps.length * 2; i < originalSteps.length; i += 1) {
-        const mapped = originalSteps[i].map(transform.mapping.slice(mapFrom));
-        mapFrom -= 1;
-        if (mapped && !transform.maybeStep(mapped).failed) {
-          const result = mapped.apply(doc);
-          transform.mapping.setMirror(mapFrom, transform.steps.length - 1);
-
-          if (!result.doc) {
-            // eslint-disable-next-line no-console
-            console.error("Error applying a step.", result.failed);
-            throw new Meteor.Error('invalid-request', "Invalid step.");
-          }
-          doc = result.doc;
-        }
-      }
-    }
 
     const timestamp = new Date();
 
-    // Save merge steps.
-    transform.steps.forEach((x, i) => {
-      if (i >= originalSteps.length) {
-        version += 1;
-        Content.documents.upsert({
-          version,
-          contentKeys: original.contentKey,
-        }, {
-          $setOnInsert: {
-            contentKeys: [original.contentKey],
-            createdAt: timestamp,
-            author: user.getReference(),
-            clientId: args.clientId,
-            step: x.toJSON(),
-          },
-        });
-      }
+    // Update forked document
+    Document.documents.update({
+      _id: fork._id,
+    }, {
+      $set: {
+        isMerged: true,
+        lastSync: fork.version,
+        updatedAt: timestamp,
+        lastActivity: timestamp,
+        title: fork.title,
+      },
     });
 
-    if (original.version < version) {
-      // Update document version
-      Document.documents.update({
-        _id: original._id,
-      }, {
-        $set: {
-          version,
-          body: doc.toJSON(),
-          updatedAt: timestamp,
-          lastActivity: timestamp,
-          title: extractTitle(doc),
-        },
-      });
-    }
+    // Update original document
+    Document.documents.update({
+      _id: original._id,
+    }, {
+      $set: {
+        version: fork.version,
+        body: fork.body,
+      },
+    });
   },
 });
 
