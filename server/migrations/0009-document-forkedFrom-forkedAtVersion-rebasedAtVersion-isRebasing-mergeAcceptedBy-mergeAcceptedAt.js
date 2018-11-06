f030a813e2d8ac9a6151f40de26889ad57e8828a
Mitar <mitar.git@tnode.com>
2018-11-06 00:20:35 -0800

    Updating migration.

--- server/migrations/0009-document-forkedFrom-forkedAtVersion-rebasedAtVersion-isRebasing-mergeAcceptedBy-mergeAcceptedAt.js
+++ server/migrations/0009-document-forkedFrom-forkedAtVersion-rebasedAtVersion-isRebasing-mergeAcceptedBy-mergeAcceptedAt.js
@@ -1,12 +1,14 @@
 import {Document} from '/lib/documents/document';
 
 class Migration extends Document.AddRequiredFieldsMigration {
-  name = "Adding forkedFrom, forkedAtVersion, rebasedAtVersion, and isRebasing fields";
+  name = "Adding forkedFrom, forkedAtVersion, rebasedAtVersion, isRebasing, mergeAcceptedBy, and mergeAcceptedAt fields";
   fields = {
     forkedFrom: null,
     forkedAtVersion: null,
     rebasedAtVersion: null,
     isRebasing: false,
+    mergeAcceptedBy: null,
+    mergeAcceptedAt: null,
   }
 }
 
